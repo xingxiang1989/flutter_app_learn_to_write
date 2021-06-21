a05f0e2d25e7861ae0842bd04fd6c62bc2d03516
xiangxing <993819762@qq.com>
2021-06-21 20:25:47 +0800

    listview 列表

--- lib/ui/home/notification/notification.dart
+++ lib/ui/home/notification/notification.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapplearntowrite/widget/notification_item_view.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 ///一个ListView，用于学习各种页面的入口
 class NotificationPage extends StatefulWidget {
@@ -8,16 +9,26 @@ class NotificationPage extends StatefulWidget {
 }
 
 class _NotificationPageState extends State<NotificationPage> {
-
-  static const Datas = const ["provider"];
+  static const Datas = const ["provider", "aa", "bb"];
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ListView.builder(itemBuilder: (BuildContext context, int index){
-        return NotificationItemView(Datas[index],tapCallback: (){
-
-        });
-      }),
+      child: ListView.separated(
+          itemCount: Datas.length,
+          // ignore: missing_return
+          separatorBuilder:(BuildContext context, int index){
+            return Divider(height: 1,color: Colors.black);
+          },
+          itemBuilder: (BuildContext context, int index) {
+            return NotificationItemView(Datas[index], tapCallback: () {
+              _goPage(context,  Datas[index]);
+            });
+          }),
     );
+  }
+
+  ///item 点击事件跳转
+  _goPage(BuildContext context,String label){
+    Fluttertoast.showToast(msg: label);
   }
 }
--- lib/widget/notification_item_view.dart
+++ lib/widget/notification_item_view.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
-class NotificationItemView extends StatelessWidget{
-
+class NotificationItemView extends StatelessWidget {
   String _label;
   GestureTapCallback tapCallback;
 
-  NotificationItemView(this._label,{this.tapCallback});
+  NotificationItemView(this._label, {this.tapCallback});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.only(bottom: 20),
-    child: Text(_label),);
+    return GestureDetector(
+      onTap: tapCallback,
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Text(_label),
+      ),
+    );
   }
 }
